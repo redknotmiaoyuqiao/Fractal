9e45ea578ba8a6236ae3d6d434c913ce1c7e3998
redknot <redknotmiaoyuqiao@gmail.com>
2016-04-14 08:47:13 +0800

    abc

--- FractalPad/app/src/main/java/com/redknot/fractalpad/DrawActivity.java
+++ FractalPad/app/src/main/java/com/redknot/fractalpad/DrawActivity.java
@@ -2,10 +2,18 @@ package com.redknot.fractalpad;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.redknot.domain.Point;
 import com.redknot.view.DrawSurfaceView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by miaoyuqiao on 16/4/12.
@@ -13,6 +21,7 @@ import com.redknot.view.DrawSurfaceView;
 public class DrawActivity extends AppCompatActivity {
 
     private DrawSurfaceView draw_surfaceview;
+    public static ArrayList<Point> points = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,29 @@ public class DrawActivity extends AppCompatActivity {
 
         String point = getIntent().getStringExtra("point");
 
+        try {
+            JSONArray ja = new JSONArray(point);
+            points.clear();
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject jo = ja.getJSONObject(i);
+                Point p = new Point((float)jo.getDouble("x"),(float)jo.getDouble("y"));
+                points.add(p);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         draw_surfaceview = (DrawSurfaceView) findViewById(R.id.draw_surfaceview);
+        draw_surfaceview.setPoints(points);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
