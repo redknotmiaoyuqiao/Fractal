a7474381fe53cbf1b2bd69c0a4892e513012a4a8
redknot <redknotmiaoyuqiao@gmail.com>
2016-04-18 16:49:29 +0800

    fractal

--- FractalAndroid/app/src/main/java/com/redknot/setting/JuliaSetting.java
+++ FractalAndroid/app/src/main/java/com/redknot/setting/JuliaSetting.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.widget.ExploreByTouchHelper;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.redknot.fractalandroid.CustomJuliaActivity;
 import com.redknot.fractalandroid.R;
@@ -23,13 +26,13 @@ public class JuliaSetting extends AppCompatActivity {
     private TextView julia_c_image_show;
     private TextView julia_c_times_show;
     private SeekBar julia_c_times;
-    private SeekBar julia_c_real;
-    private SeekBar julia_c_image;
+
+    private EditText julia_c_image_edit;
+    private EditText julia_c_real_edit;
+
 
     private Button submit;
 
-    private double real = 0;
-    private double image = 0;
     private int times = 0;
 
     @Override
@@ -37,11 +40,14 @@ public class JuliaSetting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_julia);
 
+        julia_c_image_edit = (EditText) findViewById(R.id.julia_c_image_edit);
+        //julia_c_image_edit.setText("0");
+        julia_c_real_edit = (EditText) findViewById(R.id.julia_c_real_edit);
+        //julia_c_real_edit.setText("0");
+
         julia_c_real_show = (TextView) findViewById(R.id.julia_c_real_show);
         julia_c_times_show = (TextView) findViewById(R.id.julia_c_times_show);
         julia_c_image_show = (TextView) findViewById(R.id.julia_c_image_show);
-        julia_c_real = (SeekBar) findViewById(R.id.julia_c_real);
-        julia_c_image = (SeekBar) findViewById(R.id.julia_c_image);
         julia_c_times = (SeekBar) findViewById(R.id.julia_c_times);
 
         submit = (Button) findViewById(R.id.submit);
@@ -50,6 +56,17 @@ public class JuliaSetting extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(JuliaSetting.this, CustomJuliaActivity.class);
+
+                double image = 0;
+                double real = 0;
+                try {
+                    image = Double.valueOf(julia_c_image_edit.getText().toString());
+                    real = Double.valueOf(julia_c_real_edit.getText().toString());
+                } catch (Exception e) {
+                    Toast.makeText(JuliaSetting.this, "输入出错", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 intent.putExtra("real", real);
                 intent.putExtra("image", image);
                 intent.putExtra("times", times);
@@ -57,44 +74,8 @@ public class JuliaSetting extends AppCompatActivity {
             }
         });
 
-        julia_c_real.setMax(1000);
-        julia_c_real.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                real = (progress - 500) * 0.001;
-                julia_c_real_show.setText("real : " + (progress - 500) * 0.001);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-        julia_c_image.setMax(1000);
-        julia_c_image.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                image = (progress - 500) * 0.001;
-                julia_c_image_show.setText("image : " + (progress - 500) * 0.001);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
         julia_c_times.setMax(20);
+        julia_c_times_show.setText("times : 0");
         julia_c_times.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
