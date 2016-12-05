680d28d173e8d8c668d74f76e226235bb470f274
rome753 <rome753@163.com>
2016-12-05 22:33:01 +0800

    减小Mandbrot3计算量

--- FractalAndroid/app/src/main/java/com/redknot/fractal/Mandelbrot3.java
+++ FractalAndroid/app/src/main/java/com/redknot/fractal/Mandelbrot3.java
@@ -13,7 +13,7 @@ public class Mandelbrot3 extends Fractal {
     @Override
     public int comR(int i, int j) {
         double a = 0, b = 0, d, n = 0;
-        for (; a * a + (d = b * b) < 4 && n++ < 8192; b = 2 * a * b + j / 5e4 + .06, a = a * a - d + i / 5e4 + .34)
+        for (; a * a + (d = b * b) < 4 && n++ < 1024; b = 2 * a * b + j / 5e4 + .06, a = a * a - d + i / 5e4 + .34)
             ;
         tempR = (int) (n / 4);
         return tempR;
